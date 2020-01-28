8c13358d3eb85a2f89198e0e75525ed0afcd43f5
matt imdad <mimdad@bsd20.org>
2020-01-28 10:42:56 -0600

    Initial Commit

---
+++ TopStories/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TopStories
+//
+//  Created by matt imdad on 1/28/20.
+//  Copyright © 2020 matt imdad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
