94e4665fdb5a782c51572ac7993271e15b978b96
matt imdad <mimdad@bsd20.org>
2020-01-30 10:30:39 -0600

    Added some functions for the app to display errors, and stories

--- TopStories/SourcesViewController.swift
+++ TopStories/SourcesViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class SourcesViewController: UITableViewController {
-
+    
     var sources = [[String: String]]()
     let apiKey = "5d892509a49046a087917c466fa80d09"
     
@@ -17,8 +17,48 @@ class SourcesViewController: UITableViewController {
         super.viewDidLoad()
         self.title = "News Sources"
         let query = "https://newsapi.org/v1/sources?language=en&country=us&apiKey=\(apiKey)"
+        if let url = URL(string: query) {
+            if let data = try? Data(contentsOf: url) {
+                let json = try! JSON(data: data)
+                if json["status"] == "ok" {
+                    parse(json: json)
+                    return
+                }
+            }
+        }
+        loadError()
     }
-
-
+    
+    func parse(json: JSON) {
+        for result in json["sources"].arrayValue {
+            let id = result["id"].stringValue
+            let name = result["name"].stringValue
+            let description = result["description"].stringValue
+            let source = ["id": id, "name": name, "description": description]
+            sources.append(source)
+        }
+        tableView.reloadData()
+    }
+    
+    func loadError() {
+        let alert = UIAlertController(title: "Loading Error",
+                                      message: "There was a problem loading the news feed",
+                                      preferredStyle: .actionSheet)
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return sources.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let source = sources[indexPath.row]
+          cell.textLabel?.text = source["name"]
+          cell.detailTextLabel?.text = source["description"]
+        return cell
+    }
 }
+
 
